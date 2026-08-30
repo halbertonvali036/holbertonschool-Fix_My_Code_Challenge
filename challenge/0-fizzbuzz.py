d77766e3cf47bfa4a0e6716f5889e92eb0b5320d
halbertonvali036 <13801@holbertonstudents.com>
2026-08-30 21:17:07 +0400

    dfasf

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
-if i % 3 == 0 and i % 5 == 0:
-    tmp_result.append("FizzBuzz")
-elif i % 3 == 0:
-    tmp_result.append("Fizz")
-elif i % 5 == 0:
-    tmp_result.append("Buzz")
-else:
-    tmp_result.append(str(i))
+"""
+FizzBuzz module
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    1-dən n-ə qədər ədədləri FizzBuzz qaydalarına uyğun çap edir
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            tmp_result.append("FizzBuzz")
+        elif i % 3 == 0:
+            tmp_result.append("Fizz")
+        elif i % 5 == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+
+    print(" ".join(tmp_result))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        fizzbuzz(100)
+    else:
+        fizzbuzz(int(sys.argv[1]))
